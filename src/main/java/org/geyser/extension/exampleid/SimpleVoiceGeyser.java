a7088aa3be1cc7610965c9efc8d862916ad659f8
TheodoreMeyer <94801204+TheodoreMeyer@users.noreply.github.com>
2025-01-10 16:00:36 -0500

    Update simpleVoiceGeyser.java

--- src/main/java/org/geyser/extension/exampleid/SimpleVoiceGeyser.java
+++ src/main/java/org/geyser/extension/exampleid/SimpleVoiceGeyser.java
@@ -1,4 +1,4 @@
-package org.geyser.extension.exampleid;
+package io.github.theodoremeyer.Geyser.simplevoicegeyser;
 
 import org.geysermc.event.subscribe.Subscribe;
 import org.geysermc.geyser.api.event.lifecycle.GeyserLoadResourcePacksEvent;
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 
 // The main class of your extension - must implement extension, and be in the extension.yml file.
 // see https://github.com/GeyserMC/Geyser/blob/master/api/src/main/java/org/geysermc/geyser/api/extension/Extension.java for more info on available methods
-public class ExampleExtension implements Extension {
+public class SimpleVoiceGeyser implements Extension {
 
     // Registering custom items/blocks, or adding resource packs (and basically all other events that are fired before Geyser initializes fully)
     // are done in their respective events. See below for an example:
@@ -23,7 +23,7 @@ public class ExampleExtension implements Extension {
     @Subscribe
     public void onPostInitialize(GeyserPostInitializeEvent event) {
         // example: show that your extension is loading.
-        this.logger().info("Loading example extension...");
+        this.logger().info("Loading SimpleVoiceChat for Geyser extension...");
         
         //example: accessing extension data folder
         Path exampleDataFolder = this.dataFolder();
