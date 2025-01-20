b50084d0b0eb0f8202c93ae7520ca56381aa8be1
TheodoreMeyer <94801204+TheodoreMeyer@users.noreply.github.com>
2025-01-20 16:51:10 -0500

    Update SimpleVoiceGeyser.java
    add config file and netty

--- src/main/java/org/geyser/extension/exampleid/SimpleVoiceGeyser.java
+++ src/main/java/org/geyser/extension/exampleid/SimpleVoiceGeyser.java
@@ -5,29 +5,64 @@ import org.geysermc.geyser.api.event.lifecycle.GeyserLoadResourcePacksEvent;
 import org.geysermc.geyser.api.event.lifecycle.GeyserPostInitializeEvent;
 import org.geysermc.geyser.api.extension.Extension;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Set;
 
-// The main class of your extension - must implement extension, and be in the extension.yml file.
-// see https://github.com/GeyserMC/Geyser/blob/master/api/src/main/java/org/geysermc/geyser/api/extension/Extension.java for more info on available methods
 public class SimpleVoiceGeyser implements Extension {
-
-    // Registering custom items/blocks, or adding resource packs (and basically all other events that are fired before Geyser initializes fully)
-    // are done in their respective events. See below for an example:
-    @Subscribe
-    public void onGeyserLoadResourcePacksEvent(GeyserLoadResourcePacksEvent event) {
-        logger().info("Loading: " + event.resourcePacks().size() + " resource packs.");
-        // you could add a resource pack with event.resourcePacks().add(path-to-pack)
-    }
+    
+    private static final int JETTY_PORT = 25566; // needs to get changed to get config\
+    private JettyServer jettyServer;
+    
+    public final Set<WebSocketEndpoint> endpoints = new HashSet<>();
+    public final Map<UUID, WebSocketEndpoint> endpointsByPlayer = new HashMap<>();
 
     // You can use the GeyserPostInitializeEvent to run anything after Geyser fully initialized and is ready to accept bedrock player connections.
     @Subscribe
     public void onPostInitialize(GeyserPostInitializeEvent event) {
-        // example: show that your extension is loading.
-        this.logger().info("Loading SimpleVoiceChat for Geyser extension...");
         
-        //example: accessing extension data folder
-        Path exampleDataFolder = this.dataFolder();
-        this.logger().info(exampleDataFolder.toString());
-    }
+        //show that the extension is loading.
+        this.logger().info("Loading SimpleVoiceChat for Geyser extension...");
 
+        Path ConfigPath = this.dataFolder().resolve("config.yml");
+            this.saveDefaultConfig(ConfigPath);
+
+         jettyServer = new JettyServer(JETTY_PORT, this);
+        try {
+            jettyServer.start();
+            log.info("Jetty started on port " + JETTY_PORT);
+        } catch (Exception e) {
+            log.severe("Failed to start Jetty: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+    private void saveDefaultConfig(Path ConfigPath) throws IOException {
+        if (Files.exists(ConfigPath)) {
+            return;
+        }
+        if (Files.notExists(this.dataFolder())) {
+            Files.createDirectory(this.dataFolder());
+        }
+
+        try {
+            URI uri = this.getClass().getResource("/commands.yml").toURI();
+            try (FileSystem fileSystem = FileSystems.newFileSystem(uri, new HashMap<>(), null)) {
+                Path path = fileSystem.getPath("Config.yml");
+                Files.copy(path, ConfigPath);
+            }
+        } catch (IOException | URISyntaxException ex) {
+            this.logger().error("Failed to create Config.yml!", ex);
+            this.setEnabled(false);
+        }
+    }
 }
